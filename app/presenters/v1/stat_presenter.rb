2d4858d26157d14247504ff7df73fb6cffa5dae4
Clem Bradley <clem@groupon.com>
2014-04-19 11:47:14 -0700

    format averages as three decimal floats

--- app/presenters/v1/stat_presenter.rb
+++ app/presenters/v1/stat_presenter.rb
@@ -7,11 +7,16 @@ class V1::StatPresenter
   def as_json(options = {})
     {
       at_bats: @stat.at_bats,
+      batting_average: @stat.batting_average && sprintf("%0.03f", @stat.batting_average),
       earned_runs: @stat.earned_runs,
+      games_played: @stat.games_played,
+      hit_by_pitch: @stat.hit_by_pitch,
       hits: @stat.hits,
       hits_given_up: @stat.hits_given_up,
       home_runs: @stat.home_runs,
       innings_pitched: @stat.innings_pitched,
+      on_base_percentage: @stat.on_base_percentage && sprintf("%0.03f", @stat.on_base_percentage),
+      plate_appearances: @stat.plate_appearances,
       player: V1::PlayerPresenter.new(@stat.player).as_json,
       projection: @stat.projection,
       quality_starts: @stat.quality_starts,
