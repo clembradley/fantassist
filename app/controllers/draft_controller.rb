a8c1b477d137f7239c608928533764d582394d3c
Clem Bradley <clem@groupon.com>
2014-04-23 17:24:49 -0500

    eager load player association when querying stats in draft controller

--- app/controllers/draft_controller.rb
+++ app/controllers/draft_controller.rb
@@ -2,7 +2,7 @@ class DraftController < ApplicationController
 
   def index
     #TODO: remove this temporary where clause once pagination is in place
-    stats = Stat.undrafted.where('at_bats > 400 AND home_runs > 15')
+    stats = Stat.undrafted.where('at_bats > 400 AND home_runs > 15').includes(:player)
     @stats = stats.map { |stat| V1::StatPresenter.new(stat) }
   end
 end
