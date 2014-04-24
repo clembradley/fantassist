3b2ce4334f2591668f23303ee61cf724396c8fae
Clem Bradley <clem@groupon.com>
2014-04-24 18:25:17 -0500

    add api/v1/draft_picks destroy endpoint

--- app/controllers/api/v1/draft_picks_controller.rb
+++ app/controllers/api/v1/draft_picks_controller.rb
@@ -3,12 +3,23 @@ module V1; end
 class Api::V1::DraftPicksController < ApplicationController
 
   def create
-    draft_pick = DraftPick.new(_draft_pick_params)
+    draft_pick = DraftPick.new(params.permit(:drafter_id, :player_id))
 
     if draft_pick.save
-      render json: V1::DraftPickPresenter.new(draft_pick)
+      render json: V1::DraftPickPresenter.new(draft_pick), status: :created
     else
-      render json: V1::ErrorsPresenter.new(draft_pick.errors), status: :unprocessable_entity
+      render json: V1::ErrorsPresenter.new(draft_pick.errors), status: :bad_request
+    end
+  end
+
+  def destroy
+    draft_pick = DraftPick.find_by(params.permit(:id))
+
+    if draft_pick
+      draft_pick.destroy
+      head :no_content
+    else
+      render json: {errors: ["Draft pick not found"]}, status: :bad_request
     end
   end
 
@@ -17,9 +28,5 @@ class Api::V1::DraftPicksController < ApplicationController
     draft_pick_presenters = draft_picks.map { |dp| V1::DraftPickPresenter.new(dp) }
 
     render json: draft_pick_presenters
-  end
-
-  def _draft_pick_params
-    params.permit(:drafter_id, :player_id)
   end
 end
--- spec/requests/api/v1/draft_picks_controller_spec.rb
+++ spec/requests/api/v1/draft_picks_controller_spec.rb
@@ -8,9 +8,27 @@ describe Api::V1::DraftPicksController do
 
       get '/api/v1/draft_picks'
 
+      expect(response.status).to eq(200)
       expect(json_body).to eq(expected_draft_picks.map { |dp| V1::DraftPickPresenter.new(dp) }.as_json)
     end
+  end
 
+  describe 'DELETE /api/v1/draft_picks' do
+    it 'destroys the draft pick and returns 204' do
+      draft_pick = create(:draft_pick)
+
+      delete "/api/v1/draft_picks/#{draft_pick.id}"
+
+      expect(response.status).to eq(204)
+      expect(DraftPick.find_by(id: draft_pick.id)).to be_nil
+    end
+
+    it 'returns a 400 if the draft pick to delete is not found' do
+      delete '/api/v1/draft_picks/-1'
+
+      expect(response.status).to eq(400)
+      expect(json_body).to eq(errors: ["Draft pick not found"])
+    end
   end
 
   describe 'POST /api/v1/draft_picks' do
@@ -22,12 +40,14 @@ describe Api::V1::DraftPicksController do
 
       draft_pick = DraftPick.find_by(player_id: player.id, drafter_id: drafter.id)
       expect(draft_pick).to_not be_nil
+      expect(response.status).to eq(201)
       expect(json_body).to eq(V1::DraftPickPresenter.new(draft_pick).as_json)
     end
 
-    it 'returns a 422 if the draft pick creation fails' do
+    it 'returns a 400 if the draft pick creation fails' do
       post '/api/v1/draft_picks', player_id: -1, drafter_id: -1
 
+      expect(response.status).to eq(400)
       expect(json_body).to eq(errors: ["Drafter can't be blank", "Player can't be blank"])
     end
   end
