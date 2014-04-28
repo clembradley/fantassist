55a760efdc06927a602ca5f25d76fc61e809c7d1
Clem Bradley <clem@groupon.com>
2014-04-28 10:25:01 -0400

    invert nav header colors; add google+ image to nav header

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user.persisted?
-      flash.notice = 'Signed in through Google!'
       sign_in_and_redirect user
     else
       session['devise.user_attributes'] = user.attributes
